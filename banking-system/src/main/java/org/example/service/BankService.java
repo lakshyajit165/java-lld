08423ab9e50cd86b86813f070803e4dbb095bd72
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2025-08-02 00:15:01 +0530

    Added custom exception and new methods to the bank service

--- banking-system/src/main/java/org/example/service/BankService.java
+++ banking-system/src/main/java/org/example/service/BankService.java
@@ -1,7 +1,9 @@
 package org.example.service;
 
 import org.example.commands.Command;
+import org.example.exception.AccountNotFoundException;
 import org.example.models.Account;
+import org.example.models.Transaction;
 
 import java.util.List;
 
@@ -9,10 +11,34 @@ public class BankService {
     public void executeCommand(Command command) {
         command.execute();
     }
-    public Account getAccountById(List<Account> accounts, int accountId) {
+    public Account getAccountById(List<Account> accounts, int accountId) throws AccountNotFoundException {
         List<Account> filteredAccounts = accounts.stream()
                 .filter(account -> account.getId() == accountId)
                 .toList();
+        if(filteredAccounts.isEmpty()) {
+            throw new AccountNotFoundException("Account ID " + accountId + " not found");
+        }
         return filteredAccounts.getFirst();
     }
+
+    public double getAccountBalance(List<Account> accounts, int accountId) throws AccountNotFoundException {
+        List<Account> filteredAccounts = accounts.stream()
+                .filter(account -> account.getId() == accountId)
+                .toList();
+        if(filteredAccounts.isEmpty()) {
+            throw new AccountNotFoundException("Account ID " + accountId + " not found");
+        }
+        return filteredAccounts.getFirst().getBalance();
+    }
+
+    public List<Transaction> getAllAccountTransactions(List<Account> accounts, int accountId) throws AccountNotFoundException {
+        List<Account> filteredAccounts = accounts.stream()
+                .filter(account -> account.getId() == accountId)
+                .toList();
+        if(filteredAccounts.isEmpty()) {
+            throw new AccountNotFoundException("Account ID " + accountId + " not found");
+        }
+        return filteredAccounts.getFirst().getTransactions();
+    }
+
 }
