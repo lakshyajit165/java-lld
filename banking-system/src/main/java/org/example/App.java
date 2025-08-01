261b100669a329d8b24ce14f2b37f3cf09fe2103
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2025-08-02 00:15:26 +0530

    modified main method to log all transaction details per account

--- banking-system/src/main/java/org/example/App.java
+++ banking-system/src/main/java/org/example/App.java
@@ -9,6 +9,8 @@ import org.example.models.Transaction;
 import org.example.models.TransactionType;
 import org.example.service.BankService;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -23,34 +25,45 @@ public class App
         bankService.executeCommand(new CreateAccountCommand(bank, 1, 5000));
         bankService.executeCommand(new CreateAccountCommand(bank, 2, 10000));
 
+        try {
+            bankService.executeCommand(new DepositCommand(
+                    bankService.getAccountById(bank.getAccounts(), 1), 15000
+            ));
 
-        bankService.executeCommand(new DepositCommand(
-                bankService.getAccountById(bank.getAccounts(), 1), 15000
-        ));
+            bankService.executeCommand(new TransferCommand(
+                    bankService.getAccountById(bank.getAccounts(), 1),
+                    bankService.getAccountById(bank.getAccounts(), 2),
+                    8000
+            ));
+            System.out.println("Transaction Details per account:");
+            System.out.println("-------------------------------------");
+            for(Account acc : bank.getAccounts()) {
+                System.out.println("Transactions for account: " + acc.getId());
+                List<Transaction> transactions = bankService.getAllAccountTransactions(bank.getAccounts(), acc.getId());
+                for(Transaction txn : transactions) {
+                    System.out.print("id: " + txn.getId() + " type: " + txn.getTransactionType() + " amount: $" + txn.getAmount() + " | ");
+                }
+                System.out.println();
 
-        bankService.executeCommand(new TransferCommand(
-                bankService.getAccountById(bank.getAccounts(), 1),
-                bankService.getAccountById(bank.getAccounts(), 2),
-                8000
-        ));
+                double totalCredit = acc.getTransactions()
+                        .stream()
+                        .filter(transaction -> transaction.getTransactionType() == TransactionType.CREDIT)
+                        .mapToDouble(Transaction::getAmount)
+                        .sum();
 
-        for(Account acc : bank.getAccounts()) {
-            double totalCredit = acc.getTransactions()
-                    .stream()
-                    .filter(transaction -> transaction.getTransactionType() == TransactionType.CREDIT)
-                    .mapToDouble(Transaction::getAmount)
-                    .sum();
-
-            double totalDebit = acc.getTransactions()
-                    .stream()
-                    .filter(transaction -> transaction.getTransactionType() == TransactionType.DEBIT)
-                    .mapToDouble(Transaction::getAmount)
-                    .sum();
-
-            System.out.println("Account ID: " + acc.getId());
-            System.out.println("  ➕ Total Credit: $" + totalCredit);
-            System.out.println("  ➖ Total Debit : $" + totalDebit);
-            System.out.println("  🔄 Final Balance: $" + acc.getBalance());
+                double totalDebit = acc.getTransactions()
+                        .stream()
+                        .filter(transaction -> transaction.getTransactionType() == TransactionType.DEBIT)
+                        .mapToDouble(Transaction::getAmount)
+                        .sum();
+                System.out.println("  ➕ Total Credit: $" + totalCredit);
+                System.out.println("  ➖ Total Debit : $" + totalDebit);
+                System.out.println("  🔄 Final Balance: $" + acc.getBalance());
+                System.out.println("-------------------------------------");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+
     }
 }
